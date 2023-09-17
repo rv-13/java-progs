1e6009f33097112ea150fa26c02369c94e820212
rv-13 <r131194@gmail.com>
2023-09-17 16:07:32 +0530

    arr progs

--- src/main/java/arrays/minMaxInAnArray.java
+++ src/main/java/arrays/minMaxInAnArray.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class minMaxInAnArray {
     static int findMin(int[] arr, int n) {
         int res = arr[0];
@@ -17,10 +19,21 @@ public class minMaxInAnArray {
         return res;
     }
 
+    static int findMinByStream(int[] arr, int n) {
+        return Arrays.stream(arr).min().getAsInt();
+    }
+
+    static int findMaxByStream(int[] arr, int n) {
+        return Arrays.stream(arr).max().getAsInt();
+
+    }
+
     public static void main(String[] args) {
-        int[] arr = {9, 8, 3, 21, 333, 44};
+        int[] arr = {9, 8, 3, 21, -555, 44};
         int n = arr.length;
-        System.out.println("Min:-" + findMin(arr, n));
-        System.out.println("Max:-" + findMax(arr, n));
+//        System.out.println("Min:-" + findMin(arr, n));
+//        System.out.println("Max:-" + findMax(arr, n));
+        System.out.println("Min:-" + findMinByStream(arr, n));
+        System.out.println("Max:-" + findMaxByStream(arr, n));
     }
 }
