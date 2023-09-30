96b83fc87753f90541e2fb174a0d59a36cc8652e
rv-13 <r131194@gmail.com>
2023-09-30 15:13:35 +0530

    String progs

--- src/main/java/strings/PermutationByRecurs.java
+++ src/main/java/strings/PermutationByRecurs.java
@@ -1,25 +1,32 @@
 package strings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PermutationByRecurs {
     public static void main(String[] args) {
-        String str = "ABCDEF";
+        String str = "ABCD";
         permuteString(str, "");
+
 
     }
 
-    static void permuteString(String str, String ans) {
+    static List<String> permuteString(String str, String ans) {
         if (str.length() == 0) {
             System.out.println(ans + "");
-            return;
+            return new ArrayList<>();
         }
+        List<String> ls = null;
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
 
             String remOfString = str.substring(0, i) +
                     str.substring(i + 1);
             //recursive call
-            permuteString(remOfString, ans + ch);
-        }
+            ls = new ArrayList<>();
+            ls.add(permuteString(remOfString, ans + ch).toString());
 
+        }
+        return ls;
     }
 }
